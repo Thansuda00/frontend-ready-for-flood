587e88f0fbe2e5be0555130dae02d886a2512643
Thanyathorn.J <thansuda.j@bluebik.com>
2025-08-23 13:50:58 +0700

    fix: add link

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ const bannerImages = [
 const bannerLinks = [
   'https://www.facebook.com/profile.php?id=61576044613232',
   'https://line.me/R/ti/p/@769btwlv',
-  ''
+  'https://www.facebook.com/share/16qeaMkQbu/?mibextid=wwXIf'
 ];
 
 export default function HomeScreen() {
